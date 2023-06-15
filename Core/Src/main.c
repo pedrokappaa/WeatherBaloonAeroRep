cd81749707eb6578476ed246592c742f2396fff0
JoaoPedro9400 <92300384+JoaoPedro9400@users.noreply.github.com>
2023-06-15 11:44:30 +0100

    SPI4 communication added

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -62,6 +62,7 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+<<<<<<< Updated upstream
 // SD Card Init
 
 FATFS fs;						// file system data
@@ -92,6 +93,34 @@ void ConstMsg(char* Msg, float T_PT100, float T_STM, float B1, float B2, float B
 	if(T_PT100<0 && T_STM<0){
 		sprintf(Msg, "{T1:-%.2f;T2:-%.2f;B1:%.1f;B2:%.1f;B3:%.1f}",T_PT100, T_STM, B1, B2, B3);
 	}
+=======
+uint16_t ADC_results[5];
+char MSG[42] = ""; //Message to send to arduino
+float B1 = 0; //battery 1 level
+float B2 = 0; //battery 2 level
+float B3 = 0; //battery 3 level
+float T_PT100 = 0; //Temperature provided by PT100
+float T_STM = 0;//Temperature provided by STM
+
+void Build_MSG(){
+	//builds the Message that will be sent from the STM(Master) to the Arduino(Slave)
+	sprintf(MSG, "{T1:%.2f;T2:%.2f;B1:%.1f;B2:%.1f;B3:%.1f}",T_PT100, T_STM, B1, B2, B3);
+}
+
+void Send_MSG(){
+	//Send Message from STM(Master) to Arduino(Slave) using SPI4
+
+	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET); // CS Low => initiate communication between Master and Slave
+	int i = 0;
+
+	for(i=0;i<strlen(MSG);i++){
+		char dataSend = MSG[i];
+		HAL_SPI_Transmit(&hspi4, (uint8_t*)&dataSend, 1,100); //send byte by byte
+	}
+	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET); // CS High => End communication
+
+}
+>>>>>>> Stashed changes
 
 }
 
@@ -120,11 +149,17 @@ void SendToSlave(char* MSG, int lenMSG){
   */
 int main(void)
 {
+<<<<<<< Updated upstream
 	/* USER CODE BEGIN 1 */
 	HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET); // CS High => No communication between Slave (Arduino) and Master (STM)
 	/* USER CODE END 1 */
 
 	/* MCU Configuration--------------------------------------------------------*/
+=======
+  /* USER CODE BEGIN 1 */
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET); //CS of SPI4 High => No communication between STM and Arduino
+  /* USER CODE END 1 */
+>>>>>>> Stashed changes
 
 	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
 	HAL_Init();
@@ -160,9 +195,16 @@ int main(void)
 
   	HAL_Delay(500);
 
+<<<<<<< Updated upstream
   	// Open file to write
   	if(f_open(&fil, "first.txt", FA_OPEN_ALWAYS | FA_READ | FA_WRITE) != FR_OK)
   		send_UART("Not opened");
+=======
+	HAL_TIM_Base_Start_IT(&htim1); //start timer
+
+	HAL_ADC_Start_DMA(&hadc1, (uint32_t*) ADC_results, 5);
+	// implementar sliding window average
+>>>>>>> Stashed changes
 
   	HAL_Delay(500);
 
@@ -257,6 +299,7 @@ int main(void)
 			*/
 		}
 
+<<<<<<< Updated upstream
 		//Test values
 		float T_PT100 = 20.345689; // PT100 temperature
 		float T_STM = 19.452389; // STM temperature
@@ -268,6 +311,8 @@ int main(void)
 		char MSG[42] = ""; //Message to send
 		ConstMsg(MSG, T_PT100, T_STM, B1, B2, B3); // build Message
 		SendToSlave(MSG, strlen(MSG)); //Send message MSG to Slave
+=======
+>>>>>>> Stashed changes
 
     /* USER CODE END WHILE */
 
